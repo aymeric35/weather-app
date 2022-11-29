e1d350c825839e479c29486bad22d81fdf87d58f
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-29 21:05:09 +0100

    🔧 Add conf partial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,15 @@ export default defineNuxtConfig({
         plugins: [
             svgLoader()
         ],
-        devBundler: 'legacy' // to be removed once a fix is deployed, see : https://github.com/nuxt/framework/issues/7794
+        devBundler: 'legacy', // to be removed once a fix is deployed, see : https://github.com/nuxt/framework/issues/7794
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: [
+                        '@use "@/assets/styles/conf.scss" as *;',
+                    ]
+                }
+            }
+        }
     }
 })
