bbf3ebe5110659328bfcbcb5e64b5c247ef900b8
Aymeric <aymeric.bertrand35@gmail.com>
2022-11-29 21:05:36 +0100

    🔧 Add main.scss to global imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,8 @@ export default defineNuxtConfig({
     css: [
         'primevue/resources/themes/saga-blue/theme.css',
         'primevue/resources/primevue.css',
-        'primeicons/primeicons.css'
+        'primeicons/primeicons.css',
+        '@/assets/styles/main.scss'
     ],
 	build: {
 		transpile: ['primevue']
