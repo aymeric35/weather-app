f0dde3d73f9d380c1f04a47180a2b88360227c25
Aymeric <aymeric.bertrand35@gmail.com>
2022-12-11 12:09:50 +0100

    🚨 Lint code

--- types/weather.ts
+++ types/weather.ts
@@ -1,53 +1,53 @@
 declare interface Weather {
-    coord:      Coord;
-    weather:    WeatherElement[];
-    base:       string;
-    main:       Main;
-    visibility: number;
-    wind:       Wind;
-    clouds:     Clouds;
-    dt:         number;
-    sys:        Sys;
-    timezone:   number;
-    id:         number;
-    name:       string;
-    cod:        number;
+  coord: Coord
+  weather: WeatherElement[]
+  base: string
+  main: Main
+  visibility: number
+  wind: Wind
+  clouds: Clouds
+  dt: number
+  sys: Sys
+  timezone: number
+  id: number
+  name: string
+  cod: number
 }
 
 interface Clouds {
-    all: number;
+  all: number
 }
 
 interface Coord {
-    lon: number;
-    lat: number;
+  lon: number
+  lat: number
 }
 
 interface Main {
-    temp:       number;
-    feels_like: number;
-    temp_min:   number;
-    temp_max:   number;
-    pressure:   number;
-    humidity:   number;
+  temp: number
+  feels_like: number
+  temp_min: number
+  temp_max: number
+  pressure: number
+  humidity: number
 }
 
 interface Sys {
-    type:    number;
-    id:      number;
-    country: string;
-    sunrise: number;
-    sunset:  number;
+  type: number
+  id: number
+  country: string
+  sunrise: number
+  sunset: number
 }
 
 interface WeatherElement {
-    id:          number;
-    main:        string;
-    description: string;
-    icon:        string;
+  id: number
+  main: string
+  description: string
+  icon: string
 }
 
 interface Wind {
-    speed: number;
-    deg:   number;
+  speed: number
+  deg: number
 }
