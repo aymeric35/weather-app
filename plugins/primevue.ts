3814416e21781a49082aaf4558364e6278d50f4a
Aymeric <aymeric.bertrand35@gmail.com>
2022-12-11 11:06:58 +0100

    🚨 Lint code

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,12 +1,12 @@
-import { defineNuxtPlugin } from "#app";
-import PrimeVue from "primevue/config";
-import Button from "primevue/button";
-import InputText from 'primevue/inputtext';
-import ProgressSpinner from 'primevue/progressspinner';
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import ProgressSpinner from 'primevue/progressspinner'
+import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, {ripple: true});
-    nuxtApp.vueApp.component('Button', Button);
-    nuxtApp.vueApp.component('InputText', InputText);
-    nuxtApp.vueApp.component('ProgressSpinner', ProgressSpinner);
-});
+  nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+  nuxtApp.vueApp.component('Button', Button)
+  nuxtApp.vueApp.component('InputText', InputText)
+  nuxtApp.vueApp.component('ProgressSpinner', ProgressSpinner)
+})
