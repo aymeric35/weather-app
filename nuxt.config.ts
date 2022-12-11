c0601a0e4396108bbea9f1536601d6940262ff44
Aymeric <aymeric.bertrand35@gmail.com>
2022-12-11 11:06:13 +0100

    💄 Add quicksand fontsource

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import svgLoader from 'vite-svg-loader'
 export default defineNuxtConfig({
-    app: {
-        head: {
-            link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
-            ]
-        }
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
+      ],
     },
-    css: [
-        '@unocss/reset/tailwind.css',
-        'primevue/resources/themes/saga-blue/theme.css',
-        'primevue/resources/primevue.css',
-        'primeicons/primeicons.css',
-        '@/assets/styles/main.scss'
+  },
+  css: [
+    '@unocss/reset/tailwind.css',
+    'primevue/resources/themes/vela-purple/theme.css',
+    'primevue/resources/primevue.css',
+    'primeicons/primeicons.css',
+    'primeflex/primeflex.css',
+    '@fontsource/quicksand/400.css',
+    '@fontsource/quicksand/700.css',
+    '@/assets/styles/main.scss',
+  ],
+  build: {
+    transpile: ['primevue'],
+  },
+  vite: {
+    plugins: [
+      svgLoader(),
     ],
-	build: {
-		transpile: ['primevue']
-	},
-    vite: {
-        plugins: [
-            svgLoader()
-        ],
-        devBundler: 'legacy', // to be removed once a fix is deployed, see : https://github.com/nuxt/framework/issues/7794
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: [
-                        '@use "@/assets/styles/conf.scss" as *;',
-                    ]
-                }
-            }
-        }
-    }
+    devBundler: 'legacy', // to be removed once a fix is deployed, see : https://github.com/nuxt/framework/issues/7794
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: [
+            '@use "@/assets/styles/conf.scss" as *;',
+          ],
+        },
+      },
+    },
+  },
 })
