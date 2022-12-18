9888ab2055938c41ddf62d5eb0e8f2d9077dfbb1
Aymeric <aymeric.bertrand35@gmail.com>
2022-12-18 11:54:04 +0100

    ♻️ Improve promise handling

--- app.vue
+++ app.vue
@@ -1,38 +1,46 @@
 <script lang="ts" setup>
-const value = ref('');
-const isLoading = ref(false)
-let weather = ref<Weather | undefined>()
-let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
-const delay = (delayInms: number) => {
-  return new Promise(resolve => setTimeout(resolve, delayInms));
+const city = ref('')
+const errorHandling = ref<getWeather404 | undefined>({
+  cod: '',
+  message: '',
+})
+const isLoading = ref<boolean>(false)
+const weather = ref<Weather | undefined >()
+const regionNames = new Intl.DisplayNames(['en'], { type: 'region' })
+
+const clear = () => {
+  weather.value = undefined
+  errorHandling.value = undefined
 }
 
-async function getWeather(value: string) {
-  weather.value = undefined;
-  value = value.toLowerCase();
+interface getWeather404 {
+  cod: string
+  message: string
+}
+
+async function getWeather(city: string) {
+  clear()
   try {
-    isLoading.value = true;
-    await delay(300);
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=81c72ec1a219ea59c2c74b56333ac7a8`)
-    const data = await res.json()
-    isLoading.value = false;
-    if (data.cod === '404') return alert(data.message)
-    console.log(data);
-    weather.value = data;
-  } catch (err) {
-    return err;
+    isLoading.value = true
+    await delay(500)
+    weather.value = await $fetch(`/api/weather/${city}`)
+  }
+  catch (err: any) {
+    errorHandling.value = err
+    return err
+  }
+  finally {
+    isLoading.value = false
   }
 }
-
-
 </script>
 
 <template>
   <div>
     <div>
       <div class="p-inputgroup">
-        <InputText @keypress.enter="getWeather(value)" type="text" v-model="value" placeholder="Search a city" />
-        <Button @click="getWeather(value)" label="Search" />
+        <InputText v-model="city" type="text" placeholder="Search a city" :loading="isLoading" @keypress.enter="getWeather(city)" />
+        <Button label="Search" :loading="isLoading" @click="getWeather(city)" />
       </div>
     </div>
     <div v-if="isLoading">
@@ -41,15 +49,18 @@ async function getWeather(value: string) {
     <div v-if="weather">
       <h1>{{ weather.name }}</h1>
       <h2>{{ regionNames.of(weather.sys.country) }}</h2>
-      <!-- <p>{{ weather.main.feels_like }} ˚C</p>
+      <p>{{ weather.main.feels_like }} ˚C</p>
       <p>{{ weather.main.humidity }}%</p>
       <p>{{ weather.main.pressure }} hectopascals</p>
       <p>{{ weather.main.temp }} ˚C</p>
       <p>{{ weather.main.temp_max }} ˚C</p>
-      <p>{{ weather.main.temp_min }} ˚C</p> -->
+      <p>{{ weather.main.temp_min }} ˚C</p>
+    </div>
+    <div v-if="errorHandling" class="error">
+      {{ errorHandling.message }}
     </div>
   </div>
   <div v-if="weather">
-    <WeatherWidget :temperature="weather.main.temp" :weather-icon="weather.weather[0].icon"></WeatherWidget>
+    <WeatherWidget :temperature="weather.main.temp" :weather-icon="weather.weather[0].icon" />
   </div>
 </template>
