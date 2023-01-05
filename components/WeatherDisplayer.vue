d44f3b39335e731c9abdf91dd4c9134f4d0d22f6
Aymeric <aymeric.bertrand35@gmail.com>
2023-01-05 13:58:46 +0100

    ♻️ Refactor data in its own component

--- components/WeatherDisplayer.vue
+++ components/WeatherDisplayer.vue
@@ -2,27 +2,60 @@
 const weather = useWeather()
 const errorHandling = useWeatherErrorHandling()
 const isLoading = useLoadingState()
-const regionNames = new Intl.DisplayNames(['en'], { type: 'region' })
 </script>
 
 <template>
-  <div v-if="isLoading">
-    <p>...loading</p>
-  </div>
-  <div v-if="weather">
-    <h1>{{ weather.name }}</h1>
-    <h2>{{ regionNames.of(weather.sys.country) }}</h2>
-    <p>{{ weather.main.feels_like }} ˚C</p>
-    <p>{{ weather.main.humidity }}%</p>
-    <p>{{ weather.main.pressure }} hectopascals</p>
-    <p>{{ weather.main.temp }} ˚C</p>
-    <p>{{ weather.main.temp_max }} ˚C</p>
-    <p>{{ weather.main.temp_min }} ˚C</p>
-  </div>
-  <div v-if="errorHandling" class="error">
-    {{ errorHandling.message }}
-  </div>
-  <div v-if="weather">
-    <WeatherWidget :temperature="weather.main.temp" :weather-icon="weather.weather[0].icon" />
-  </div>
+  <section>
+    <div v-if="isLoading" class="loader-container">
+      <span class="loader" />
+    </div>
+    <div v-if="errorHandling" class="error">
+      {{ errorHandling.message }}
+    </div>
+    <XyzTransition xyz="fade left">
+      <div v-if="weather">
+        <WeatherWidget :temperature="weather.main.temp" :weather-icon="weather.weather[0].icon" />
+      </div>
+    </XyzTransition>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 40rem;
+  margin-top: 2rem;
+}
+
+.loader-container {
+  position: absolute;
+  top:50%;
+  left:50%;
+  translate: -50% -50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 5px solid #FFF;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
