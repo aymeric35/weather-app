53ddeca459b0ca1bace41517226c685bcfdb5b54
Aymeric <aymeric.bertrand35@gmail.com>
2023-01-08 22:06:06 +0100

    ✨ Add forecast states

--- composables/states.ts
+++ composables/states.ts
@@ -1,5 +1,8 @@
-export const useWeather = () => useState<Weather | undefined>('weather', () => undefined)
+// export const useWeather = () => useState<Weather | undefined>('weather', () => undefined)
+export const useForecast = () => useState<Forecast | undefined>('forecast', () => undefined)
 export const useCity = () => useState<string>('city', () => '')
+export const useSelectedForecastDate = () => useState<number>('selectedForecastDate', () => Date.now())
+export const useForecastDataByDate = () => useState<ForecastData | undefined>('forecastDataByDate', () => undefined)
 export const useWeatherErrorHandling = () => useState<getWeather404 | undefined>('weatherErrorHandling', () => ({
   cod: '',
   message: '',
